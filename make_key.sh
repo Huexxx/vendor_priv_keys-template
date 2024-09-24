6431e4b34a3e69e1776d6d609918d33e0efb1704
Huexxx <huexxx@gmail.com>
2024-09-24 11:23:44 +0200

    Make standard

--- make_key.sh
+++ make_key.sh
@@ -6,4 +6,4 @@
 set -u
 bash <(sed "s/2048/${2:-2048}/;/Enter password/,+1d" ../../../development/tools/make_key) \
     $1 \
-    '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+    '/C=ES/ST=Madrid/L=Madrid/O=Android/OU=Android/CN=Android/emailAddress=huexxx@gmail.com'
